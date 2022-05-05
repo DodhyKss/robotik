c2c11649d9e103aa73e4692de188b79b9a304cea
DodhyKss <65852126+DodhyKss@users.noreply.github.com>
2022-05-05 20:48:08 +0700

    Update algoritma_parsing_data_arduino.ino

--- algoritma_parsing_data_arduino.ino
+++ algoritma_parsing_data_arduino.ino
@@ -1,6 +1,6 @@
-String stringOne;
-String data[3];
-int x=0;
+String stringOne; // variabel ini digunakan untuk menyimpan data yang telah diambil dari serial dalam bentuk string
+String data[3]; //array ini digunakan untuk menyimpan 3 data yang telah diparsing dalam bentuk string
+int x=0; // ketiga data ini x,y,z adalah variabel yang digunakan untuk menyimpan 3 data yang telah diparsing dalam bentuk integer
 int y=0;
 int z=0;
 
@@ -24,8 +24,13 @@ void loop() {
 void bacaDataCam(){
     if(Serial.available()>0){
       delay(10);
-      stringOne = Serial.readString();
-      int q = 0;
+      stringOne = Serial.readString(); // mengambil data dari serial dan disimpan dalam bentuk string
+      int q = 0; // variabel ini digunakan sebagai counter
+     
+     /*ketiga for di bawah digunakan untuk memisahkan data yang telah disimpan di stringOne, misalkan data yang diterima adalah 128#223@543$
+     maka tersebut dari index 0,1,2 akan disimpan pada array data[] pada index 0 (pertama) dan begitupun untuk 3 index berikutnya hinggan data[] index 2(ketiga)*/
+     
+     /*karena data yang dikirim adalah 3 maka untuk memisahkan data tersebut kita menambahkan simbol yaitu #,@ dan $*/
       for(int i=0;i<stringOne.length();i++){
         if(stringOne[i] == '#'){
           q = i;
